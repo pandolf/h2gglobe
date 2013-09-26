3db816675a0a9a5db56073a144855779f433f834
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-09-26 16:44:17 +0200

    using updated version of filloptree

--- PhotonAnalysis/src/MassFactorizedMvaAnalysis.cc
+++ PhotonAnalysis/src/MassFactorizedMvaAnalysis.cc
@@ -875,18 +875,9 @@ bool MassFactorizedMvaAnalysis::AnalyseEvent(LoopAll& l, Int_t jentry, float wei
             if (diPhoSys != 0)
                 name = diPhoSys->name();
 	    
-            if (!isSyst)
-//		fillOpTree(l, lead_p4, sublead_p4, vtxProb, diphoton_index, diphoton_id, phoid_mvaout_lead, phoid_mvaout_sublead, weight, 
-//			   mass, sigmaMrv, sigmaMwv, Higgs, diphobdt_output, category, VBFevent, myVBF_Mjj, myVBFLeadJPt, 
-//			   myVBFSubJPt, nVBFDijetJetCategories, isSyst, "no-syst");
-        fillOpTree(l, lead_p4, sublead_p4, &smeared_pho_energy[0], -2, diphoton_index, diphoton_id, -2, -2, weight, evweight,
-               mass, -1, -1, Higgs, -2, category, VBFevent, myVBF_Mjj, myVBFLeadJPt,
-               myVBFSubJPt, nVBFDijetJetCategories, isSyst, "no-syst");
-            else
-        fillOpTree(l, lead_p4, sublead_p4, &smeared_pho_energy[0], -2, diphoton_index, diphoton_id, -2, -2, weight, evweight,
-               mass, -1, -1, Higgs, -2, category, VBFevent, myVBF_Mjj, myVBFLeadJPt,
-               myVBFSubJPt, nVBFDijetJetCategories, isSyst, name);
-	    
+            if(!isSyst)
+              fillOpTree(l, lead_p4, sublead_p4,  diphoton_index, diphoton_id, weight, evweight, mass, category );
+
         }
 
         if (PADEBUG) std::cout << " Diphoton Category " <<category <<std::endl;
