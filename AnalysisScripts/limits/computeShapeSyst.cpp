64e2a0e1e4f89e6c9e2280bba690dce2d9ce2c45
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-10-09 18:45:36 +0200

    first implementation (yet dummy) of fitting

--- AnalysisScripts/limits/computeShapeSyst.cpp
+++ AnalysisScripts/limits/computeShapeSyst.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+#include "TCanvas.h"
+#include "TH2D.h"
 #include "TF1.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TGraph.h"
 
 
 float getScalingFactor(TF1* f1, float deltaM);
+TF1* getExpoFromFit( const std::string& fileName );
 
 int main() {
 
@@ -11,9 +17,10 @@ int main() {
   TF1* flat = new TF1("uniform", "[0]", 10.0, 180.);
   flat->SetParameter(0, 1./80.);
 
-  TF1* expo = new TF1("expo", "exp([0]+[1]*x)", 100., 180.);
-  expo->SetParameter(0, 8. );
-  expo->SetParameter(1, -0.1 );
+  TF1* expo = getExpoFromFit("../batchOutput4/histograms_CMS-HGG.root");
+  //TF1* expo = new TF1("expo", "exp([0]+[1]*x)", 100., 180.);
+  //expo->SetParameter(0, 8. );
+  //expo->SetParameter(1, -0.1 );
 
   float sf_flat = getScalingFactor(flat, 3.);
   float sf_expo = getScalingFactor(expo, 3.);
@@ -37,5 +44,54 @@ float getScalingFactor(TF1* f1, float deltaM) {
   float alpha = sR/(s1+s2);
 
   return alpha;
+
+}
+
+
+TF1* getExpoFromFit( const std::string& fileName ) {
+
+  TFile* file = TFile::Open(fileName.c_str());
+  TTree* tree = (TTree*)file->Get("Data");
+
+  int category;
+  float PhotonsMass;
+  float thqLD_lept;
+
+  tree->SetBranchAddress( "category", &category );
+  tree->SetBranchAddress( "PhotonsMass", &PhotonsMass );
+  tree->SetBranchAddress( "thqLD_lept", &thqLD_lept );
+
+  int nentries=tree->GetEntries();
+  TH1D* h1_data = new TH1D( "data", "", 80., 100., 180.);
+
+  for( unsigned ientry=0; ientry<nentries; ++ientry ) {
+
+    tree->GetEntry(ientry);
+
+    if( category!=11 ) continue;
+    if( PhotonsMass>115. && PhotonsMass<135. ) continue;
+    if( thqLD_lept>0.25 ) continue; //inverted LD cut
+
+    h1_data->Fill(PhotonsMass);
+
+  }
+
+  TF1* expo = new TF1("expo", "exp([0]+[1]*x)", 100., 180.);
+  h1_data->Fit(expo, "RL");
+
+  h1_data->SetMarkerStyle(20);
+  h1_data->SetMarkerSize(2.);
+
+  TCanvas* c1 = new TCanvas("c1", "", 600, 600);
+  c1->cd();
+
+  TH2D* h2_axes = new TH2D("axes", "", 10, 100., 180., 10, 0., 2.);
+  
+  h2_axes->Draw();
+  h1_data->Draw("P same");
+
+  c1->SaveAs("prova.eps");
+
+  return expo;
 
 }
