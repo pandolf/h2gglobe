4caa90bc8e9a7d8cc1bb84f286d047a65d50b8b2
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2013-12-13 15:02:52 +0100

    added some lines

--- AnalysisScripts/limits/combine_signif.sh
+++ AnalysisScripts/limits/combine_signif.sh
@@ -1,2 +1,4 @@
+#combine -M HybridNew datacard_thq_leptonic_noLDcut.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_noLDcut.log
 combine -M HybridNew datacard_thq_leptonic.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic.log
 combine -M HybridNew datacard_thq_hadronic.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_hadronic.log
+combine -M HybridNew datacard_thq_hadronic_metCut.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_hadronic_metCut.log
--- AnalysisScripts/limits/combine_signif_obs.sh
+++ AnalysisScripts/limits/combine_signif_obs.sh
@@ -1,7 +1,14 @@
 
+cat datacard_thq_leptonic.txt | sed 's/observation 0/observation 1/' >! datacard_thq_leptonic_obs1.txt
 cat datacard_thq_leptonic.txt | sed 's/observation 0/observation 2/' >! datacard_thq_leptonic_obs2.txt
-cat datacard_thq_hadronic.txt | sed 's/observation 0/observation 2/' >! datacard_thq_hadronic_obs2.txt
+cat datacard_thq_leptonic.txt | sed 's/observation 0/observation 3/' >! datacard_thq_leptonic_obs3.txt
+cat datacard_thq_leptonic.txt | sed 's/gmN 1/gmN 0/' >! datacard_thq_leptonic_0events.txt
+cat datacard_thq_leptonic_0events.txt | sed 's/observation 0/observation 2/' >! datacard_thq_leptonic_0events_obs2.txt
+cat datacard_thq_hadronic.txt | sed 's/observation 0/observation 4/' >! datacard_thq_hadronic_obs4.txt
 
+#combine -M HybridNew datacard_thq_leptonic_0events_obs2.txt --significance --saveToys --fullBToys --saveHybridResult -T 10000 -i 13 -s13 >&! log_signif_obs2_leptonicChannel_0events.log
+combine -M HybridNew datacard_thq_leptonic_obs1.txt --significance --saveToys --fullBToys --saveHybridResult -T 10000 -i 13 -s13 >&! log_signif_obs1_leptonicChannel.log
 combine -M HybridNew datacard_thq_leptonic_obs2.txt --significance --saveToys --fullBToys --saveHybridResult -T 10000 -i 13 -s13 >&! log_signif_obs2_leptonicChannel.log
-combine -M HybridNew datacard_thq_hadronic_obs2.txt --significance --saveToys --fullBToys --saveHybridResult -T 10000 -i 13 -s13 >&! log_signif_obs2_hadronicChannel.log
+combine -M HybridNew datacard_thq_leptonic_obs3.txt --significance --saveToys --fullBToys --saveHybridResult -T 10000 -i 13 -s13 >&! log_signif_obs3_leptonicChannel.log
+#combine -M HybridNew datacard_thq_hadronic_obs4.txt --significance --saveToys --fullBToys --saveHybridResult -T 10000 -i 13 -s13 >&! log_signif_obs4_hadronicChannel.log
 
