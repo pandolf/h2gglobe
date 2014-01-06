77ecfede34f7782dfbc6171a2ff07e10c81c9015
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-01-06 12:46:28 +0100

    first commit

---
+++ AnalysisScripts/ulstudies/copyToAN.sh
@@ -0,0 +1,5 @@
+epstopdf checkUL2_hybrid.eps
+mv checkUL2_hybrid.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/ul_vs_alpha_hybr.pdf
+
+epstopdf checkUL2.eps
+mv checkUL2.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/ul_vs_alpha_freq.pdf
