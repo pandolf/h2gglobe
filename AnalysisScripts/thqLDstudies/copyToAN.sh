c81bf89308cdb3bc5cb093727582e30d4c1fa672
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-02-03 12:10:02 +0100

    prima che micheli faccia le syst

--- AnalysisScripts/thqLDstudies/copyToAN.sh
+++ AnalysisScripts/thqLDstudies/copyToAN.sh
@@ -1,40 +1,52 @@
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/RoC.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/RoC.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/RoC.eps
+mv RoCs_andShit_${1}/RoC.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/deltaEta_lept_qJet_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/deltaEta_lept_qJet_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/RoC_withBDT2.eps
+mv RoCs_andShit_${1}/RoC_withBDT2.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/deltaPhi_top_higgs_LDcut_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/deltaPhi_top_higgs_LDcut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/deltaEta_lept_qJet_leptonic.eps
+mv RoCs_andShit_${1}/deltaEta_lept_qJet_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/deltaPhi_top_higgs_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/deltaPhi_top_higgs_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/deltaPhi_top_higgs_LDcut_leptonic.eps
+mv RoCs_andShit_${1}/deltaPhi_top_higgs_LDcut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/lept_charge_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/lept_charge_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/deltaPhi_top_higgs_leptonic.eps
+mv RoCs_andShit_${1}/deltaPhi_top_higgs_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/njets_InsideEtaCut_LDcut_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/njets_InsideEtaCut_LDcut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/lept_charge_leptonic.eps
+mv RoCs_andShit_${1}/lept_charge_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/njets_InsideEtaCut_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/njets_InsideEtaCut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/nbjets_medium_leptonic.eps
+mv RoCs_andShit_${1}/nbjets_medium_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/njets_OutsideEtaCut_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/njets_OutsideEtaCut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/nbjets_loose_leptonic.eps
+mv RoCs_andShit_${1}/nbjets_loose_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/njets_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/njets_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/njets_InsideEtaCut_LDcut_leptonic.eps
+mv RoCs_andShit_${1}/njets_InsideEtaCut_LDcut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/qJetEta_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/qJetEta_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/njets_InsideEtaCut_leptonic.eps
+mv RoCs_andShit_${1}/njets_InsideEtaCut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/topMt_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/topMt_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/njets_OutsideEtaCut_leptonic.eps
+mv RoCs_andShit_${1}/njets_OutsideEtaCut_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/bJetPt_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/bJetPt_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/njets_leptonic.eps
+mv RoCs_andShit_${1}/njets_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
-epstopdf RoCs_andShit_qJetEtaFix_newLD_v1/thqLD_lept_leptonic.eps
-mv RoCs_andShit_qJetEtaFix_newLD_v1/thqLD_lept_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+epstopdf RoCs_andShit_${1}/qJetEta_leptonic.eps
+mv RoCs_andShit_${1}/qJetEta_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+
+epstopdf RoCs_andShit_${1}/topMt_leptonic.eps
+mv RoCs_andShit_${1}/topMt_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+
+epstopdf RoCs_andShit_${1}/bJetPt_leptonic.eps
+mv RoCs_andShit_${1}/bJetPt_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+
+epstopdf RoCs_andShit_${1}/thqLD_lept_leptonic.eps
+mv RoCs_andShit_${1}/thqLD_lept_leptonic.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
+
+epstopdf RoCs_andShit_${1}/fractionVsCut_thqLD_lept.eps
+mv RoCs_andShit_${1}/fractionVsCut_thqLD_lept.pdf ~/tdr2/notes/AN-13-392/trunk/figures/leptonic/
 
 
