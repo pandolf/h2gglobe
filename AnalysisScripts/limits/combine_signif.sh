d16601fec79f1021cdeecee00cb71fb71089204f
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-02-03 12:09:59 +0100

    prima che micheli faccia le syst

--- AnalysisScripts/limits/combine_signif.sh
+++ AnalysisScripts/limits/combine_signif.sh
@@ -1,4 +1,13 @@
-#combine -M HybridNew datacard_thq_leptonic_noLDcut.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_noLDcut.log
-combine -M HybridNew datacard_thq_leptonic.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic.log
-combine -M HybridNew datacard_thq_hadronic.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_hadronic.log
-combine -M HybridNew datacard_thq_hadronic_metCut.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_hadronic_metCut.log
+#combine -M HybridNew datacard_thq_leptonic_${1}_noLDcut.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}_noLDcut.log
+combine -M HybridNew datacard_thq_leptonic_${1}.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}.log
+combine -M HybridNew datacard_thq_leptonic_${1}_extraTTH.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}_extraTTH.log
+#combine -M HybridNew datacard_thq_hadronic_${1}.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_hadronic_${1}.log
+#combine -M HybridNew datacard_thq_hadronic_${1}_metCut.txt --significance  --expectedFromGrid=0.5  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_hadronic_${1}_metCut.log
+
+# +1 sigma
+combine -M HybridNew datacard_thq_leptonic_${1}.txt --significance  --expectedFromGrid=0.84  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}_p1s.log
+combine -M HybridNew datacard_thq_leptonic_${1}_extraTTH.txt --significance  --expectedFromGrid=0.84  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}_extraTTH_p1s.log
+
+# -1 sigma
+combine -M HybridNew datacard_thq_leptonic_${1}.txt --significance  --expectedFromGrid=0.16  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}_m1s.log
+combine -M HybridNew datacard_thq_leptonic_${1}_extraTTH.txt --significance  --expectedFromGrid=0.16  --saveToys --fullBToys --saveHybridResult -T 2000 -i 10 -s13 >&! log_signif_thq_leptonic_${1}_extraTTH_m1s.log
